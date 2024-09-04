ee561b9193713d9c1cefaa397bf4a0502763677b
ashutoshdas1212 <ashutoshdas1212@gmail.com>
2024-09-04 18:07:14 +0530

    all

---
+++ src/components/Recruiter/Recruiter.tsx
@@ -0,0 +1,25 @@
+
+import * as S from './style';
+
+const Recruiter = () => (
+  <S.Container>
+    <S.Thumbnail>
+    
+    </S.Thumbnail>
+    <S.Description>
+      <h4>
+        Work in the Netherlands
+        
+      </h4>
+      <p>
+        Hi! I'm Ashutosh Das from Doghouse IT Recruitment and I'm looking for
+        skilled Software Engineers like you. If you wish to move abroad,{' '}
+        <a href="https://www.linkedin.com/in/ashutosh-das-5736b51aa/">
+          <b>follow me on Linkedin.</b>
+        </a>
+      </p>
+    </S.Description>
+  </S.Container>
+);
+
+export default Recruiter;
